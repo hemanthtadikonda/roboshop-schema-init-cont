0340a798529af616171246cd148271c44b79b4b2
hemanth <hemanthtadikonda29@gmail.com>
2023-11-01 19:18:18 +0530

    Docker file changes schema setup

--- run.sh
+++ run.sh
@@ -3,13 +3,11 @@
 git clone https://github.com/hemanthtadikonda/$COMPONENT
 cd $COMPONENT/schema
 
-if [ "$DB_TYPE" == "mysql"]; then
+if [ "$DB_TYPE" == "docdb" ]; then
   aws ssm get-parameter  --name sql.${env}.schema_setup --with-decryption | jq .Parameter.Value | sed -e 's/"//g' <$COMPONENT.sql
 fi
 
-if ["$DB_TYPE" == "docdb"]; then
+if [ "$DB_TYPE" == "docdb" ]; then
   curl -o https://truststore.pki.rds.amazonaws.com/global/global-bundle.pem
   aws ssm get-parameter  --name docdb.${env}.schema_setup --with-decryption | jq .Parameter.Value | sed -e 's/"//g' <$COMPONENT.js
 fi
-
-
