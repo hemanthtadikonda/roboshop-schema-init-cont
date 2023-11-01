95628e7af0f0d8f854d4642860feaa56e26cf735
hemanth <hemanthtadikonda29@gmail.com>
2023-11-01 19:58:29 +0530

    Docker file changes schema setup

--- run.sh
+++ run.sh
@@ -10,6 +10,7 @@ fi
 
 if [ "$DB_TYPE" == "docdb" ]; then
   curl -o rds-combined-ca-bundle.pem https://truststore.pki.rds.amazonaws.com/global/global-bundle.pem
-  mongo --host dev-docdb-cluster.cluster-cec9yfajj5ic.us-east-1.docdb.amazonaws.com:27017 --sslCAFile rds-combined-ca-bundle.pem --username docdbadmin --password roboshop1234 <$COMPONENT.js
+  mongo --ssl --host dev-docdb-cluster.cluster-cec9yfajj5ic.us-east-1.docdb.amazonaws.com:27017 --sslCAFile rds-combined-ca-bundle.pem --username docdbadmin --password roboshop1234 <$COMPONENT.js
   #aws ssm get-parameter  --name docdb.${env}.schema_setup --with-decryption | jq .Parameter.Value | sed -e 's/"//g' <$COMPONENT.js
+
 fi
